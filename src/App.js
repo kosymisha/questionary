1b62150b912e474d432666a63639e59e1a7add5d
Mikhail Kosy <mikhail.kosy@itechart-group.com>
2020-12-21 09:31:46 +0300

    app styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from "./Header";
 import LeftNav from "./LeftNav";
+import Nav from "./Nav";
 import "./App.css";
 
 const App = () => (
     <div>
-        <Header/>
-        <LeftNav/>
+        <Nav/>
+        <div className="container">
+            <LeftNav/>
+            <div className="content">
+                content
+            </div>
+        </div>
     </div>
 );
 
